8da742dbdbd8cbd8c99e2d312fbe69289f40fe5a
KyungeunOh <oooo8962@naver.com>
2022-09-15 17:25:29 +0900

    15552.java 빠른 A+B

---
+++ 15552.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            bw.write((Integer.parseInt(st.nextToken())) + (Integer.parseInt(st.nextToken())) + "\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
